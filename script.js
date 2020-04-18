5dc87898dc447dfeb391fc365bd04c633311a7b9
Vitaly <zakazaps@gmail.com>
2020-04-18 14:22:38 +0500

    add form

--- script.js
+++ script.js
@@ -116,7 +116,83 @@ setClock('timer', deadline);
         document.body.style.overflow = '';
     }); 
 
+//Form-popap
 
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Наш менеджер свяжется с Вами в течении часа',
+        failure: 'Что-то пошло не так...'
+    };
 
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
 
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader ('Content-Type', 'application/x-www-form-urlencoded');
+        
+        let formData = new FormData(form);
+        request.send(formData);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status ==200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+    });
+
+// Form contact
+
+let messageContact = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Наш менеджер свяжется с Вами в течении часа',
+    failure: 'Что-то пошло не так...'
+};
+
+let formContact = document.querySelector('.contact-form'),
+    inputContact = form.getElementsByTagName('input'),
+    statusMessageContact = document.createElement('div');
+
+    statusMessageContact.classList.add('status');
+
+formContact.addEventListener('submit', function(event) {
+    event.preventDefault();
+    formContact.appendChild(statusMessageContact);
+
+    let request = new XMLHttpRequest();
+    request.open('POST', 'server.php');
+    request.setRequestHeader ('Content-Type', 'application/x-www-form-urlencoded');
+    
+    let formData = new FormData(form);
+    request.send(formData);
+
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState < 4) {
+            statusMessageContact.innerHTML = messageContact.loading;
+        } else if (request.readyState === 4 && request.status ==200) {
+            statusMessageContact.innerHTML = messageContact.success;
+        } else {
+            statusMessageContact.innerHTML = messageContact.failure;
+        }
+    });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+});
 });
